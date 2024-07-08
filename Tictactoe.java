1c663b1960391c185d6b61a46bd3b469635aecb7
Suryadeep2004 <146474801+Suryadeep2004@users.noreply.github.com>
2024-07-08 19:41:46 +0530

    Update Tictactoe.java

--- Tictactoe.java
+++ Tictactoe.java
@@ -1,15 +1,39 @@
 import java.util.Scanner;
 
-class Board {
-    private static final int SIDE = 3;
-    private char[][] board;
+public class TicTacToe {
+    static final int COMPUTER = 1;
+    static final int HUMAN = 2;
+    static final int SIDE = 3;
+    static final char COMPUTERMOVE = 'O';
+    static final char HUMANMOVE = 'X';
 
-    public Board() {
-        board = new char[SIDE][SIDE];
-        initialize();
+    public static void main(String[] args) {
+        char[][] board = new char[SIDE][SIDE];
+        initialize(board);
+        showInstructions();
+
+        int whoseTurn = getPlayerChoice();
+        playTicTacToe(board, whoseTurn);
     }
 
-    public void initialize() {
+    static void showBoard(char[][] board) {
+        System.out.printf("\t\t\t %c | %c | %c \n", board[0][0], board[0][1], board[0][2]);
+        System.out.println("\t\t\t-----------");
+        System.out.printf("\t\t\t %c | %c | %c \n", board[1][0], board[1][1], board[1][2]);
+        System.out.println("\t\t\t-----------");
+        System.out.printf("\t\t\t %c | %c | %c \n\n", board[2][0], board[2][1], board[2][2]);
+    }
+
+    static void showInstructions() {
+        System.out.println("\nChoose a cell numbered from 1 to 9 as below and play\n");
+        System.out.println("\t\t\t 1 | 2 | 3 ");
+        System.out.println("\t\t\t-----------");
+        System.out.println("\t\t\t 4 | 5 | 6 ");
+        System.out.println("\t\t\t-----------");
+        System.out.println("\t\t\t 7 | 8 | 9 \n");
+    }
+
+    static void initialize(char[][] board) {
         for (int i = 0; i < SIDE; i++) {
             for (int j = 0; j < SIDE; j++) {
                 board[i][j] = '*';
@@ -17,31 +41,15 @@ class Board {
         }
     }
 
-    public void show() {
-        for (int i = 0; i < SIDE; i++) {
-            for (int j = 0; j < SIDE; j++) {
-                System.out.print("\t" + board[i][j]);
-                if (j < SIDE - 1) System.out.print(" |");
-            }
-            System.out.println();
-            if (i < SIDE - 1) System.out.println("\t-----------");
+    static void declareWinner(int whoseTurn) {
+        if (whoseTurn == COMPUTER) {
+            System.out.println("COMPUTER has won");
+        } else {
+            System.out.println("HUMAN has won");
         }
-        System.out.println();
     }
 
-    public boolean isCellEmpty(int x, int y) {
-        return board[x][y] == '*';
-    }
-
-    public void makeMove(int x, int y, char move) {
-        board[x][y] = move;
-    }
-
-    public boolean isGameOver() {
-        return rowCrossed() || columnCrossed() || diagonalCrossed();
-    }
-
-    private boolean rowCrossed() {
+    static boolean rowCrossed(char[][] board) {
         for (int i = 0; i < SIDE; i++) {
             if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != '*') {
                 return true;
@@ -50,7 +58,7 @@ class Board {
         return false;
     }
 
-    private boolean columnCrossed() {
+    static boolean columnCrossed(char[][] board) {
         for (int i = 0; i < SIDE; i++) {
             if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != '*') {
                 return true;
@@ -59,7 +67,7 @@ class Board {
         return false;
     }
 
-    private boolean diagonalCrossed() {
+    static boolean diagonalCrossed(char[][] board) {
         if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != '*') {
             return true;
         }
@@ -69,96 +77,14 @@ class Board {
         return false;
     }
 
-    public char[][] getBoard() {
-        return board;
-    }
-}
-
-class Game {
-    private static final char COMPUTERMOVE = 'O';
-    private static final char HUMANMOVE = 'X';
-    private static final int SIDE = 3;
-    private Board board;
-    private Scanner scanner;
-
-    public Game() {
-        board = new Board();
-        scanner = new Scanner(System.in);
+    static boolean gameOver(char[][] board) {
+        return rowCrossed(board) || columnCrossed(board) || diagonalCrossed(board);
     }
 
-    public void showInstructions() {
-        System.out.println("Choose a cell numbered from 1 to 9 as below and play:");
-        System.out.println("\t 1 | 2 | 3 ");
-        System.out.println("\t-----------");
-        System.out.println("\t 4 | 5 | 6 ");
-        System.out.println("\t-----------");
-        System.out.println("\t 7 | 8 | 9 ");
-        System.out.println();
-    }
-
-    public void playTicTacToe(int whoseTurn) {
-        int moveIndex = 0, x = 0, y = 0;
-        board.initialize();
-        showInstructions();
-        board.show();
-
-        while (!board.isGameOver() && moveIndex != SIDE * SIDE) {
-            int n;
-            if (whoseTurn == 1) {
-                n = bestMove(board.getBoard(), moveIndex);
-                x = n / SIDE;
-                y = n % SIDE;
-                board.makeMove(x, y, COMPUTERMOVE);
-                System.out.println("COMPUTER has put a " + COMPUTERMOVE + " in cell " + (n + 1));
-                board.show();
-                moveIndex++;
-                whoseTurn = 2;
-            } else if (whoseTurn == 2) {
-                System.out.print("You can insert in the following positions: ");
-                for (int i = 0; i < SIDE; i++) {
-                    for (int j = 0; j < SIDE; j++) {
-                        if (board.isCellEmpty(i, j)) {
-                            System.out.print((i * 3 + j) + 1 + " ");
-                        }
-                    }
-                }
-                System.out.println();
-                System.out.print("Enter the position: ");
-                n = scanner.nextInt();
-                n--;
-                x = n / SIDE;
-                y = n % SIDE;
-                if (board.isCellEmpty(x, y) && n < 9 && n >= 0) {
-                    board.makeMove(x, y, HUMANMOVE);
-                    System.out.println("HUMAN has put a " + HUMANMOVE + " in cell " + (n + 1));
-                    board.show();
-                    moveIndex++;
-                    whoseTurn = 1;
-                } else if (!board.isCellEmpty(x, y) && n < 9 && n >= 0) {
-                    System.out.println("Position is occupied, select any one place from the available places.");
-                } else if (n < 0 || n > 8) {
-                    System.out.println("Invalid position.");
-                }
-            }
-        }
-        if (!board.isGameOver() && moveIndex == SIDE * SIDE) {
-            System.out.println("It's a draw.");
-        } else {
-            declareWinner(whoseTurn);
-        }
-    }
-
-    private void declareWinner(int whoseTurn) {
-        if (whoseTurn == 1) {
-            System.out.println("HUMAN has won");
-        } else {
-            System.out.println("COMPUTER has won");
-        }
-    }
-
-    private int minimax(char[][] board, int depth, boolean isAI) {
+    static int minimax(char[][] board, int depth, boolean isAI) {
         int score = 0;
         int bestScore = 0;
+
         if (gameOver(board)) {
             if (isAI) {
                 return -10;
@@ -204,43 +130,102 @@ class Game {
         }
     }
 
-    private boolean gameOver(char[][] board) {
-        return rowCrossed(board) || columnCrossed(board) || diagonalCrossed(board);
-    }
-
-    private boolean rowCrossed(char[][] board) {
-        for (int i = 0; i < SIDE; i++) {
-            if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != '*') {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean columnCrossed(char[][] board) {
-        for (int i = 0; i < SIDE; i++) {
-            if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != '*') {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean diagonalCrossed(char[][] board) {
-        if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != '*') {
-            return true;
-        }
-        if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != '*') {
-            return true;
-        }
-        return false;
-    }
-
-    private int bestMove(char[][] board, int moveIndex) {
+    static int bestMove(char[][] board, int moveIndex) {
         int x = -1, y = -1;
         int score = 0, bestScore = -999;
+
         for (int i = 0; i < SIDE; i++) {
             for (int j = 0; j < SIDE; j++) {
                 if (board[i][j] == '*') {
                     board[i][j] = COMPUTERMOVE;
-                    score = minimax(board
+                    score = minimax(board, moveIndex + 1, false);
+                    board[i][j] = '*';
+                    if (score > bestScore) {
+                        bestScore = score;
+                        x = i;
+                        y = j;
+                    }
+                }
+            }
+        }
+        return x * 3 + y;
+    }
+
+    static void playTicTacToe(char[][] board, int whoseTurn) {
+        Scanner scanner = new Scanner(System.in);
+        int moveIndex = 0, x = 0, y = 0;
+
+        showBoard(board);
+
+        while (!gameOver(board) && moveIndex != SIDE * SIDE) {
+            int n;
+            if (whoseTurn == COMPUTER) {
+                n = bestMove(board, moveIndex);
+                x = n / SIDE;
+                y = n % SIDE;
+                board[x][y] = COMPUTERMOVE;
+                System.out.printf("COMPUTER has put a %c in cell %d\n\n", COMPUTERMOVE, n + 1);
+                showBoard(board);
+                moveIndex++;
+                whoseTurn = HUMAN;
+            } else if (whoseTurn == HUMAN) {
+                System.out.print("You can insert in the following positions : ");
+                for (int i = 0; i < SIDE; i++) {
+                    for (int j = 0; j < SIDE; j++) {
+                        if (board[i][j] == '*') {
+                            System.out.print((i * 3 + j) + 1 + " ");
+                        }
+                    }
+                }
+                System.out.println("\n\nEnter the position = ");
+                n = scanner.nextInt();
+                n--;
+                x = n / SIDE;
+                y = n % SIDE;
+
+                if (board[x][y] == '*' && n < 9 && n >= 0) {
+                    board[x][y] = HUMANMOVE;
+                    System.out.printf("\nHUMAN has put a %c in cell %d\n\n", HUMANMOVE, n + 1);
+                    showBoard(board);
+                    moveIndex++;
+                    whoseTurn = COMPUTER;
+                } else if (board[x][y] != '*' && n < 9 && n >= 0) {
+                    System.out.println("\nPosition is occupied, select any one place from the available places\n\n");
+                } else if (n < 0 || n > 8) {
+                    System.out.println("Invalid position\n");
+                }
+            }
+        }
+
+        if (!gameOver(board) && moveIndex == SIDE * SIDE) {
+            System.out.println("It's a draw\n");
+        } else {
+            if (whoseTurn == COMPUTER) {
+                whoseTurn = HUMAN;
+            } else if (whoseTurn == HUMAN) {
+                whoseTurn = COMPUTER;
+            }
+            declareWinner(whoseTurn);
+        }
+
+        scanner.close();
+    }
+
+    static int getPlayerChoice() {
+        Scanner scanner = new Scanner(System.in);
+        char choice;
+        String cont;
+        do {
+            System.out.print("Do you want to start first? (y/n) : ");
+            choice = scanner.next().charAt(0);
+
+            if (choice == 'n' || choice == 'N') {
+                return COMPUTER;
+            } else if (choice == 'y' || choice == 'Y') {
+                return HUMAN;
+            } else {
+                System.out.println("Invalid choice");
+            }
+        } while (true);
+    }
+}
